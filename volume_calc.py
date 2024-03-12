832a384b7fd52b2e13618e2dbf4691beedf24f4c
mycraftTF <seungjunlee1006@gmail.com>
2024-03-12 16:39:46 +0900

    Create volume_calc.py
    오픈소스SW와 파이썬 프로그래밍 02분반 2주차 과제 
    -박스의 부피계산

---
+++ volume_calc.py
@@ -0,0 +1,12 @@
+from decimal import Decimal
+
+x = float(input("가로길이:"))
+y = float(input("세로길이:"))
+z = float(input("높이:"))
+
+Vol = x*y*z
+DecVol = Decimal(str(x)) * Decimal(str(y)) * Decimal(str(z))
+
+print("박스의 넓이는(by Float):" + str(Vol) + "입니다.")
+print("박스의 넓이는(by Decimal):" + str(DecVol) + "입니다.")
+
